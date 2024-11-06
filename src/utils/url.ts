c37fd6c922c712522277aeb070415f794c9290c9
Lucas Yang <yangchenshin77@gmail.com>
2024-11-06 13:35:44 +0800

    Add url util

---
+++ src/utils/url.ts
@@ -0,0 +1,36 @@
+export function urlWithParams(url: string, params: Record<string, any>, baseParams?: Record<string, any>) {
+  const paramsString = urlParams(params, baseParams)
+  return `${url}${paramsString ? '?' : ''}${paramsString}`
+}
+
+export function urlParams(params: Record<string, any>, baseParams?: Record<string, any>) {
+  let resultParams = baseParams
+    ? mergeUrlParams(baseParams, params)
+    : params
+
+  return Object.entries(resultParams)
+    .map(([key, value]) => {
+      if (Array.isArray(value)) {
+        return value.map(v => `${key}=${encodeURIComponent(v)}`).join('&')
+      } else if (value) {
+        return `${key}=${encodeURIComponent(value)}`
+      }
+    })
+    .filter(Boolean)
+    .join('&')
+}
+
+export function mergeUrlParams<
+  Params extends Record<string, any> = Record<string, any>
+>(baseParams: Params, userParams: Partial<Params>): Params {
+  return Object
+    .keys(baseParams)
+    .reduce((result, key) => {
+      if (Array.isArray(baseParams[key])) {
+        result[key] = Array.from(new Set([...baseParams[key], ...(userParams[key] || [])]))
+      } else {
+        result[key] = userParams[key] ?? baseParams[key]
+      }
+      return result
+    }, {} as Record<string, any>) as Params
+}
