55a7b1851032a3523d9614b2869d9f8ac9c45e1c
Lucas Yang <yangchenshin77@gmail.com>
2025-05-26 15:25:28 +0800

    Update astro config

--- astro.config.ts
+++ astro.config.ts
@@ -3,5 +3,9 @@ import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   site: 'http://localhost:4321',
+  trailingSlash: 'never',
+  build: {
+    format: 'file',
+  },
   adapter: vercel(),
 })
